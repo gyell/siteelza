8d0bd3ae2f6c05fe9fe6a712fc2faffb6492106b
gyell <38677211+gyell@users.noreply.github.com>
2025-12-12 15:41:52 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from './components/Header';
+import { Header } from './components/HeaderComp';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Services } from './components/Services';
